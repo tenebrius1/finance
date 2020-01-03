788e4b1956d1a47667b5c9d1e98df72bbf751070
Blackspecter <57551677+tenebrius1@users.noreply.github.com>
2020-01-03 17:19:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+from helper.helpers import apology, login_required, lookup, usd
 
 # Configure application
 app = Flask(__name__)
